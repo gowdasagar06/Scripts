47d9420850ead25eb57746ea926c066e0d2f3143
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-02-21 11:43:25 +0530

    Create jenkins-docker-helm-kubectl-awscli-terraform.sh

---
+++ Ubuntu/jenkins-docker-helm-kubectl-awscli-terraform.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Update package lists
+sudo apt update
+
+# Install dependencies
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add current user to docker group to run Docker commands without sudo
+sudo usermod -aG docker $USER
+
+# Install Jenkins dependencies (OpenJDK 11)
+sudo apt install -y openjdk-11-jdk
+
+# Add Jenkins repository key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+# Install Jenkins
+sudo apt update
+sudo apt install -y jenkins
+
+# Start Jenkins service
+sudo systemctl start jenkins
+
+# Enable Jenkins service to start on boot
+sudo systemctl enable jenkins
+
+# Display initial admin password for Jenkins
+echo "Initial admin password for Jenkins:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+#helm install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
+
+#kubectl install
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version
+
+#aws install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+#terraform install
+sudo apt install wget
+wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+unzip terraform_1.0.7_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform --version 
